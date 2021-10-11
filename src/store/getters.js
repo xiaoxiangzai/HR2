85597acaa2d3c2a6173112e898da332c64581f42
xiaoxiangzai <ergou580@163.com>
2021-10-11 15:00:56 +0800

    人资项目初始化

---
+++ src/store/getters.js
@@ -0,0 +1,9 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device
+// 删除的
+  // token: state => state.user.token,
+  // avatar: state => state.user.avatar,
+  // name: state => state.user.name
+}
+export default getters
